a2b7a43c288dd7c1941463bac7677b8529be9b3f
Harish Ojha <harish.ojha.456@gmail.com>
2026-01-01 20:06:32 +0000

    added the text file output for desmos3d

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Line.h"
 #include "Plane.h"
 #include "Utility.h"
+#include <fstream>
 
 int main(int, char **){
 
@@ -24,4 +25,51 @@ int main(int, char **){
 
     bool result = Utility::PointInTriangleOrNot(trianglePoints, intersectionPoint);
     std::cout << "and the line crosses the triangle or not:" << result << std::endl;
+
+
+
+    const std::string filePath = "../data/output.txt"; 
+    std::ofstream outputFile(filePath);
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening the file!" << std::endl;
+        return 1; // Return an error code
+    }
+
+
+
+
+    // line
+    // outputFile << "\\ \\left(1,2,3\\right)+t\\left(4,1,2\\right)"<<std::endl;
+    outputFile << "\\ \\left("
+    <<line.origin.x<<","<< line.origin.y<<","<<line.origin.z
+    <<"\\right)+t\\left("<<
+    line.direction.x<<","<<line.direction.y<<","<<line.direction.z
+    <<"\\right)"<<std::endl;
+
+    // plane
+    // outputFile << "4\\left(x-2\\right)+2\\left(y-5\\right)+8\\left(z-8\\right)=0" << std::endl;
+
+    // triangle
+    // outputFile << "\\operatorname{triangle}\\left(\\left(2,-1,0\\right),\\left(2,2,0\\right),\\left(0,0,2\\right)\\right)" << std::endl;
+    outputFile << "\\operatorname{triangle}\\left(\\left("
+    <<trianglePoints[0]->x<<","<<trianglePoints[0]->y<<","<<trianglePoints[0]->z
+    <<"\\right),\\left("
+    <<trianglePoints[1]->x<<","<<trianglePoints[1]->y<<","<<trianglePoints[1]->z
+    <<"\\right),\\left("
+    <<trianglePoints[2]->x<<","<<trianglePoints[2]->y<<","<<trianglePoints[2]->z
+    <<"\\right)\\right)" << std::endl;
+
+    // point of intersection
+    // outputFile << "(6.88235,3.47059,5.94118)" << std::endl;
+    outputFile << "("
+    <<intersectionPoint.x<<","<<intersectionPoint.y<<","<<intersectionPoint.z
+    <<")" << std::endl;
+    // outputFile << "This is some text output in C++." << std::endl;
+    // outputFile << "You can write numbers too: " << 12345 << std::endl;
+
+    // 4. Close the file
+    outputFile.close();
+
+    std::cout << "Data successfully written to output.txt" << std::endl;
+
 };
