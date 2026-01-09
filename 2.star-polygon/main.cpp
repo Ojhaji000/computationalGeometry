0343fe08f4b0d3a3dd8c065f6f6bd70e26a1ac6a
Harish Ojha <harish.ojha.456@gmail.com>
2026-01-09 10:47:44 +0000

    refactoring the readability

--- 2.star-polygon/main.cpp
+++ 2.star-polygon/main.cpp
@@ -1,75 +1,100 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
+#include "../common/Vec3.h"
+#include "../common/Utility.h"
 
-struct Point {
-    double x, y;
+enum PolygonPointIterationDirection
+{
+    clockWise,
+    counterClockWise,
+    denegerate
 };
 
-// its a great way to way to find the how i am going around
-// the points od the polygon.
-// usually when we are given the polygon points, either it is in clockwise series
-// or counterclockwise. That's how we tranverse it, going either of the two
-// ways. The below function tells that direction.
+PolygonPointIterationDirection FindIterationDirection(const std::vector<Vec3>polygon){
+    Vec3 pointA = polygon[0], pointB = polygon[1], pointC = polygon[2];
+    Vec3 vectorAB = pointB - pointA;
+    Vec3 vectorCB = pointB - pointC;
+    Vec3 resultantVector = vectorAB.cross(vectorCB);
+    if(resultantVector.z>0){
+        return counterClockWise;
+    }else if(resultantVector.z<0){
+        return clockWise;
+    }else{
+        std::cout << "the polygon is degenerate(collinear)" << std::endl;
+        return denegerate;
+    }
+}
+
+bool insideHalfPlane(const Vec3 pointA, const Vec3 pointB, const Vec3 pointInQuestion, const PolygonPointIterationDirection &iterationDir)
+{  
+    // usually when we are given the polygon points, either it is in clockwise series
+    // or counterclockwise. That's how we tranverse it, going either of the two
+    // ways. The below function tells that direction.
 // when we are given points, namely A & B, and to form an edge, we do it by
-// forming a vector which B-A, going from A to B. Similarly C-B for the edge
-// between B and C going from B to C.
-//
+    // forming a vector which B-A, going from A to B. Similarly C-B for the edge
+    // between B and C going from B to C.
+    //
 // and to check the direction of point iteration in for loop is whether
-// clockwise or counter-clockwise on the 2d plane, we could use the cross product
-//
+    // clockwise or counter-clockwise on the 2d plane, we could use the cross product
+    //
 // in polygon all the points are collinear, and they are in 2D plane, so the cross
 // product's resulting vector either point to +ve z direction or -ve z direction
-//
-// using the right hand thumb rule the, helps us in the resulting direction orientation
-//
-// so when we go counter-clockwise, the resultant vector's direction is +ve z and
-// vice-versa
-//
-// For the cross product the common origin for the two vecctors is necessary.
+    //
+    // using the right hand thumb rule the, helps us in the resulting direction orientation
+    //
+    // so when we go counter-clockwise, the resultant vector's direction is +ve z and
+    // vice-versa
+    //
+    // For the cross product the common origin for the two vecctors is necessary.
 // So from any  three consecutive points in the polygon: A, B, C;
-// forming two vectors:
-// (A-B), going from B to A 
-// and 
-// (C-B), going from B to C.
-// and in cross product, we will only focus on the sign of k-cap (or z component)
-// look the image in the current directory for clarity 
+    // forming two vectors:
+    // (A-B), going from B to A 
+    // and 
+    // (C-B), going from B to C.
+    // and in cross product, we will only focus on the sign of k-cap (or z component)
+    // look the image in the current directory for clarity
+    Vec3 pointC = pointInQuestion;
+    Vec3 vectorAB = pointB - pointA; // point
+    Vec3 vectorCB = pointB - pointC;
+    Vec3 resultantVector = vectorAB.cross(vectorCB);
 
-// double direction()
-double cross(Point a, Point b, Point c) {
-    return (b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x);
-}
-
-bool insideHalfPlane(Point a, Point b, Point p) {
-    return cross(a, b, p) >= 0; // assuming CCW polygon
-}
-
-double polygonArea(const vector<Point>& poly) {
-    double area = 0;
-    int n = poly.size();
-    for (int i = 0; i < n; i++) {
-        int j = (i + 1) % n;
-        area += poly[i].x * poly[j].y - poly[j].x * poly[i].y;
+    double coefficientOfZcomponentOfCrossProductresultant = resultantVector.z;
+    switch (iterationDir)
+    {
+        case clockWise:
+            return coefficientOfZcomponentOfCrossProductresultant <= 0;
+            break;
+        case counterClockWise:
+            return coefficientOfZcomponentOfCrossProductresultant >= 0;
+            break;
     }
-    return fabs(area) / 2.0;
 }
 
-// Return kernel polygon (list of points)
-vector<Point> computeKernel(const vector<Point>& poly) {
-    vector<Point> kernel = {
+// double polygonArea(const vector<Vec3>& poly) {
+//     double area = 0;
+//     int n = poly.size();
+//     for (int i = 0; i < n; i++) {
+//         int j = (i + 1) % n;
+//         area += poly[i].x * poly[j].y - poly[j].x * poly[i].y;
+//     }
+//     return fabs(area) / 2.0;
+// }
+
+std::vector<Vec3> computeKernel(const std::vector<Vec3>& poly, const PolygonPointIterationDirection &iterationDir) {
+    std::vector<Vec3> kernel = { // largest bounding box(kernel), which will be clipped over and over again
         { -1e9, -1e9 }, { 1e9, -1e9 },
         { 1e9,  1e9 }, { -1e9,  1e9 }
     };
 
     for (int i = 0; i < poly.size(); i++) {
-        Point a = poly[i];
-        Point b = poly[(i+1) % poly.size()];
-        vector<Point> newKernel;
+        Vec3 a = poly[i];
+        Vec3 b = poly[(i+1) % poly.size()];
+        std::vector<Vec3> newKernel;
 
-        for (int j = 0; j < kernel.size(); j++) {
-            Point curr = kernel[j];
-            Point next = kernel[(j+1) % kernel.size()];
-            bool currInside = insideHalfPlane(a, b, curr);
-            bool nextInside = insideHalfPlane(a, b, next);
+        for (int j = 0; j < kernel.size(); j++) { 
+            Vec3 curr = kernel[j];
+            Vec3 next = kernel[(j+1) % kernel.size()];
+            bool currInside = insideHalfPlane(a, b, curr, iterationDir);
+            bool nextInside = insideHalfPlane(a, b, next, iterationDir);
 
             if (currInside) newKernel.push_back(curr);
             if (currInside ^ nextInside) {
@@ -84,9 +109,9 @@ vector<Point> computeKernel(const vector<Point>& poly) {
 
                 double det = A1*B2 - A2*B1;
                 if (fabs(det) > 1e-9) {
-                    double x = (B2*C1 - B1*C2) / det;
-                    double y = (A1*C2 - A2*C1) / det;
-                    newKernel.push_back({x,y});
+                    float x = (float)(B2*C1 - B1*C2) / det;
+                    float y = (float)(A1*C2 - A2*C1) / det;
+                    newKernel.push_back({x,y,0});
                 }
             }
         }
@@ -97,19 +122,26 @@ vector<Point> computeKernel(const vector<Point>& poly) {
 }
 
 int main() {
-    vector<Point> polygon = {
+    std::vector<Vec3> polygon = {
         {0,0}, {6,0}, {6,2}, {1,2}, {4,5},{3,3},{6,3},{4,6},{0,6}
     };
-//right),\left(3,3\right),\left(6,3\right),\left(4,6\right),\left(0,6\right)
-    vector<Point> kernel = computeKernel(polygon);
+    if(polygon.size()<3){
+        std::cout << "not a polygon" << std::endl;
+        return 0;
+    }
+    PolygonPointIterationDirection iterationDir = FindIterationDirection(polygon);
+    if(iterationDir == denegerate)
+        return;
+    std::vector<Vec3> kernel = computeKernel(polygon, iterationDir);
 
     if (kernel.empty()) {
-        cout << "Polygon is NOT star-shaped." << endl;
+        std::cout << "Polygon is NOT star-shaped." << std::endl;
     } else {
-        cout << "Polygon is star-shaped. Kernel coordinates:" << endl;
+        std::cout << "Polygon is star-shaped. Kernel coordinates:" <<std:: endl;
         for (auto &p : kernel) {
-            cout << "(" << p.x << ", " << p.y << ")" << endl;
+            std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
         }
-        cout << "Kernel area = " << polygonArea(kernel) << endl;
+        // cout << "Kernel area = " << polygonArea(kernel) << endl;
     }
+    Utility::TextFileGenerator(kernel, polygon)
 }
---
+++ common/TextFileGenerator.h
@@ -0,0 +1,6 @@
+#pragma once
+// namespace
+// design patterns
+
+
+class TextFileGeneraor
